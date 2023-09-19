d37c7612d31d69805df1e9f54d4ab0bb8f837e81
sadiq22 <sadiqameen2009@gmail.com>
2023-09-19 12:02:44 +0500

    sdfsdf

---
+++ myCalculationLibrary/src/main/java/com/example/mycalculationlibrary/calculation.kt
@@ -0,0 +1,11 @@
+package com.example.mycalculationlibrary
+
+class calculation {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun min(a: Int, b: Int): Int {
+        return a - b
+    }
+}
